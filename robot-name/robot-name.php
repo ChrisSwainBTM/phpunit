f511e9ed9429a99a9d929e4ecd0a98541fddd398
Chris Swain <chrisswain@Chriss-MacBook-Pro.local>
2018-09-03 23:34:33 -0600

    robot names

--- robot-name/robot-name.php
+++ robot-name/robot-name.php
@@ -10,8 +10,6 @@ class Robot {
         $this->letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 		$this->numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 		$this->reset();
-		$this->i = 0;
-		$i = 0;
     }
 	
     public function getName() {
@@ -19,27 +17,26 @@ class Robot {
     }
 	
 	public function reset() {
-		foreach($this->letters as $v) {
-			while($this->i < 2) {
-				$this->i++;
-				$charOne = array_pop($this->letters);
-				shuffle($this->letters);
-				$charTwo = array_pop($this->letters);
-			}
+		shuffle($this->letters);
+		$charOne = $this->letters[0];
+		shuffle($this->letters);
+		$charTwo = $this->letters[0];
+		shuffle($this->numeric);
+		$charThree = $this->numeric[0];
+		shuffle($this->numeric);
+		$charFour = $this->numeric[0];
+		shuffle($this->numeric);
+		$charFive = $this->numeric[0];
+
+		$newRobotName = $charOne.$charTwo.$charThree.$charFour.$charFive;
+		
+		if(!in_array($newRobotName, self::$names)) {
+			$this->name = $newRobotName;
+			self::$names[] = $this->name;
+		} else {
+			$this->reset();
 		}
-		foreach($this->numeric as $v) {
-			while($this->i < 3) {
-				$this->i++;
-				$charThree = array_pop($this->numeric);
-				shuffle($this->numeric);
-				$charFour = array_pop($this->numeric);
-				$charFive = array_pop($this->numeric);
-			}
-		}
-	
-		$this->name = $charOne.$charTwo.$charThree.$charFour.$charFive;
-		$this->reset();
-	 }
+		
   	}	
-	
+}	
 	
