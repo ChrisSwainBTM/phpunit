142a4cbfc9f32c44c1396ca033364ef7e5bd69b8
Chris Swain <chrisswain@Chriss-MacBook-Pro.local>
2018-09-10 11:20:06 -0600

    progress, unicode anagrams still an issue

--- anagram/anagram.php
+++ anagram/anagram.php
@@ -1,56 +1,62 @@
 <?php 
 
-// $words = ['tan', 'stand', 'at'];
-// $needle = ['ant'];
-// // //
+// $words = ['ΒΓΑ', 'ΒΓΔ', 'γβα'];
+// $needle = 'ΑΒΓ';
+// // // //
 // print "hello world";
 
-function detectAnagrams($words, $needle) {
+function detectAnagrams(string $needle, array $words) {
 	// $needleArray[] = $needle;
 	// $wordsArray[] = $words;
-	$array = array();
-	if(!is_array($words)) {
-		$wordsA = array();
-		$wordsA .= array($words);
-		// print Butthole;
-	}
+	$wordsA = array();
+	$needleLower = strtolower($needle);
+	$needleChars = str_split($needleLower, 1);
+	asort($needleChars);
+	$needleJoined = implode('', $needleChars);
+	// if(!is_array($words)) {
+	// 	$wordsA = array();
+	// 	$wordsA .= array($words);
+	// 	// print 'Butthole';
+	// }
 
 	// print gettype($words);
-	// if(!is_array($words)) {
+	// if(!is_array($words)) {	
 	// 	$words = array($words);
 	// }
-	  	if(is_array($words)) {
+	
 		foreach($words as $k => $v) {
+			if(strtolower($words[$k]) === strtolower($needle)) {
+				continue;
+			}
 			
-			// print_r($words[$k]);
-		
-			if(strlen($words[$k]) == strlen($needle[$k])) {
+			$utf = (utf8_decode($words[$k]));
+			// print_r($utf);
+			
+			if(strlen($words[$k]) != strlen(utf8_decode($words[$k]))){
+				$json = json_encode($words[$k]);
+				print_r($json);
+			}
+
+			if(strlen($words[$k]) === strlen($needle)) {
+				
 				// print 'strlenghts are equal';
-			    $needleChars = str_split($needle[$k], 1);
-			    $needleLength = strlen($needle[$k]);
-			    $wordChars = str_split($words[$k], 1);
+			    // $needleLength = strlen($needle);
+				$wordsLower = strtolower($words[$k]);
+			    $wordChars = str_split($wordsLower, 1);
+				asort($wordChars);
+				
+			    $wordsJoined = implode('', $wordChars);
 				//print_r($needleChars);
 				//print_r($wordChars);
-	            $diff = array_diff($needleChars, $wordChars);
 				// print_r($diff) .'hello!';
-				if(empty($diff)){
-					$wordsA = array();
-					$wordsA = array($words[$k]);
-					print_r($wordsA);
-				} else{
-					return $array;
-				}	
-			
-			} else {
-				return $array;
-				// print 'dick';
+				if($needleJoined === $wordsJoined && !in_array(strtolower($words[$k]),  array_map('strtolower', $wordsA))) {
+					$wordsA[] = $words[$k];
+				}
 			}
 		}	
-	} else {
-		return $array;
-	}
-	// print_r($words);
-	
+
+	return($wordsA);
+
 }
 
-detectAnagrams($words, $needle);
+// detectAnagrams($words, $needle);
