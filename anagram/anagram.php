a1a34c2231e8d89d7f055b601f4051c055a21e08
Chris Swain <chrisswain@Chriss-MacBook-Pro.local>
2018-09-11 17:52:55 -0600

    clean up formatting

--- anagram/anagram.php
+++ anagram/anagram.php
@@ -5,7 +5,8 @@
 
 // print "hello world";
 
-function detectAnagrams(string $needle, array $words) {
+function detectAnagrams(string $needle, array $words)
+{
 	$wordsA = array();
 	$wordsB = array();
 	$NeedleArray = array();
@@ -19,75 +20,88 @@ function detectAnagrams(string $needle, array $words) {
 	$needleJoined = implode('', $needleChars);
 
 	// print gettype($words);
-	
-		foreach($words as $k => $v) {
-			if(strtolower($words[$k]) === strtolower($needle)) {
-				continue;
-			}
-			
-        if (strlen($words[$k]) != strlen(utf8_decode($words[$k]))) {
-			if(mb_strlen($words[$k]) != strlen($needle)) {
-			//$words[$k] = utf8_decode($words[$k]);
-			//$needle = utf8_decode($needle);
-			$mbWords = mb_strtolower($words[$k],  mb_detect_encoding($words[$k]));
-			$wordsSplit = str_split_unicode($mbWords, 1);
-			$needleSplit = str_split_unicode($needleMb, 1);
-				$coll = collator_create( 'en_US' );
-					collator_sort($coll, $wordsSplit);
-					//$wordsSplit = array_values($wordsSplit);
-					collator_sort($coll, $needleSplit);
 
+	foreach($words as $k => $v) {
+		if (strtolower($words[$k]) === strtolower($needle)) {
+			continue;
+		}
 
-		    $needleJoined = implode('', $needleSplit);
-			$wordsJoined = implode('', $wordsSplit);
-			//print_r($needleJoined);
-				//print_r($wordsJoined);
-			
-				if($needleJoined === $wordsJoined && !in_array(strtolower($words[$k]),  array_map('strtolower', $wordsB))) {
+		if (strlen($words[$k]) != strlen(utf8_decode($words[$k]))) {
+			if (mb_strlen($words[$k]) != strlen($needle)) {
+
+				// $words[$k] = utf8_decode($words[$k]);
+				// $needle = utf8_decode($needle);
+
+				$mbWords = mb_strtolower($words[$k], mb_detect_encoding($words[$k]));
+				$wordsSplit = str_split_unicode($mbWords, 1);
+				$needleSplit = str_split_unicode($needleMb, 1);
+				$coll = collator_create('en_US');
+				collator_sort($coll, $wordsSplit);
+
+				// $wordsSplit = array_values($wordsSplit);
+
+				collator_sort($coll, $needleSplit);
+				$needleJoined = implode('', $needleSplit);
+				$wordsJoined = implode('', $wordsSplit);
+
+				// print_r($needleJoined);
+				// print_r($wordsJoined);
+
+				if ($needleJoined === $wordsJoined && !in_array(strtolower($words[$k]) , array_map('strtolower', $wordsB))) {
 					$wordsB[] = $words[$k];
-					//print 'fuck you';
-				}
-			}	
 
-			//}
-			
+					// print 'fuck you';
+
+				}
+			}
+
+			// }
 
 		}
-			elseif(strlen($words[$k]) === strlen($needle)) {
-				
-				//print 'strlenghts are equal';
-			    // $needleLength = strlen($needle);
-				$wordsLower = strtolower($words[$k]);
-			    $wordChars = str_split($wordsLower, 1);
-				//print_r ($wordChars, mb_detect_encoding);
-				asort($wordChars);
-				
-			    $wordsJoined = implode('', $wordChars);
-				//print_r($needleChars);
-				//print_r($wordChars);
-				// print_r($diff) .'hello!';
-				if($needleJoined === $wordsJoined && !in_array(strtolower($words[$k]),  array_map('strtolower', $wordsB))) {
-					$wordsB[] = $words[$k];
-				}
+		elseif (strlen($words[$k]) === strlen($needle)) {
+
+			// print 'strlenghts are equal';
+			// $needleLength = strlen($needle);
+
+			$wordsLower = strtolower($words[$k]);
+			$wordChars = str_split($wordsLower, 1);
+
+			// print_r ($wordChars, mb_detect_encoding);
+
+			asort($wordChars);
+			$wordsJoined = implode('', $wordChars);
+
+			// print_r($needleChars);
+			// print_r($wordChars);
+			// print_r($diff) .'hello!';
+
+			if ($needleJoined === $wordsJoined && !in_array(strtolower($words[$k]) , array_map('strtolower', $wordsB))) {
+				$wordsB[] = $words[$k];
 			}
+		}
+	}
 
-		}	
+	return ($wordsB);
 
-	return($wordsB);
-	//print_r($wordsC);
+	// print_r($wordsC);
 
 }
 
-function str_split_unicode($str, $l = 0) {
-    if ($l > 0) {
-        $ret = array();
-        $len = mb_strlen($str, "UTF-8");
-        for ($i = 0; $i < $len; $i += $l) {
-            $ret[] = mb_substr($str, $i, $l, "UTF-8");
-        }
-        return $ret;
-    }
-    return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
+function str_split_unicode($str, $l = 0)
+{
+	if ($l > 0) {
+		$ret = array();
+		$len = mb_strlen($str, "UTF-8");
+		for ($i = 0; $i < $len; $i+= $l) {
+			$ret[] = mb_substr($str, $i, $l, "UTF-8");
+		}
+
+		return $ret;
+	}
+
+	return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
 }
+
+// detectAnagrams($words, $needle);
 
 // detectAnagrams($words, $needle);
